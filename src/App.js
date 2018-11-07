74faa29c312171f341e4f4767f15e685d187cb5e
manuel <manuel@sahores.com>
2018-11-07 13:10:54 -0300

    Starting implementation of the Burguer Builder container

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurguerBuilder />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class BurguerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+        <>
+            <div>Burguer</div>
+            <div>Build Controls</div>
+        </>
+    );
+    }
+}
+ 
+export default BurguerBuilder;
