9ac422c8b37eec8b74262618ebba64668d190e0a
manuel <manuel@sahores.com>
2018-11-08 16:19:02 -0300

    adding ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,21 +2,44 @@
 import Burger from "../../components/Burger/Burger";
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.6,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 1.4
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 0,
             bacon: 0,
             cheese: 0,
-            meat: 5
-        }
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: oldPrice + priceAddition
+        })
     }
 
     render() { 
         return (
         <>
             <Burger ingredients={this.state.ingredients} />
-            <BuildControls />
+            <BuildControls ingredientAdded={this.addIngredientHandler} />
         </>
     );
     }
