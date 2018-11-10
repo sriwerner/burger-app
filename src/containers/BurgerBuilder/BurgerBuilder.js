44131bb91f4189bd3e58f06ede1c9d3b96692489
manuel <manuel@sahores.com>
2018-11-10 13:07:40 -0300

    Adding the price to the prder summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,6 +93,7 @@ class BurgerBuilder extends Component {
         <>
             <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                 <OrderSummary 
+                    price={this.state.totalPrice}
                     ingredients={this.state.ingredients} 
                     purchaseCancelled={this.purchaseCancelHandler}
                     purchaseContinued={this.purchaseContinueHandler}
